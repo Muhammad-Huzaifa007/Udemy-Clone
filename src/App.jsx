7e484bd0746196cf1b483ddacb6c7dbf1739da03
Muhammad-Huzaifa007 <huzaifaa.muhammad007@gmail.com>
2025-02-27 17:55:35 +0500

    Latest Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./Components/Footer";
 import Footer_end from "./Components/Footer_end";
 import Footer_navbar from "./Components/Footer_navbar";
 import Logos from "./Components/Logos";
-import Main from "./Components/main";
+import Main from "./Components/Main";
 import Navbar from "./Components/Navbar";
 import Trending from "./Components/Trending";
 import Trending_body from "./Components/Trending_body";
